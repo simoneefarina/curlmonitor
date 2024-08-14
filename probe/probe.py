20893cb2b3328c5c15242b941d3cf7e3715bacc4
simomiry <farinsimone2001@gmail.com>
2024-08-14 16:34:37 +0200

    Modified the probe to use urrllib instead of subprocess

--- probe/probe.py
+++ probe/probe.py
@@ -1,129 +1,104 @@
 #!/usr/bin/env python
-import json
-#imports
-import subprocess
+# imports
 import typing
-import validators
-from mooncloud_driver import abstract_probe,atom,result,entrypoint
+import copy
+
+from urllib import request
+from urllib.error import URLError, HTTPError
+from mooncloud_driver import abstract_probe, atom, result, entrypoint
+
 
 class Probe(abstract_probe.AbstractProbe):
 
-    #Command to execute
-    #-I: Fetch the headers only
-    CMD_COMMAND = "curl -I {host}"
-
-    #Result map
+    # Creation of the result map
     RESULT_MAP = {
         result.INTEGER_RESULT_TRUE: {
             'pretty_result': "Test executed successfully",
             'base_extra_data': {}
         },
         result.INTEGER_RESULT_FALSE: {
-            'pretty_result': "Error occurred during the test execution",
+            'pretty_result': "An error occurred during test execution",
             'base_extra_data': {
-                'Error': "{e}"
+                'error': "{e}"
             }
         },
         result.INTEGER_RESULT_TARGET_CONNECTION_ERROR: {
-            'pretty_result': "Target connection error",
+            'pretty_result': "Failed to connect to the target",
             'base_extra_data': {
-                'Error': "{e}"
-            }
-        },
-        result.INTEGER_RESULT_INPUT_ERROR: {
-            'pretty_result': "Input error",
-            'base_extra_data': {
-                'Error': "{e}"
+                'error': "{e}"
             }
         },
         result.INTEGER_RESULT_MOON_CLOUD_ERROR: {
-            'pretty_result': "Assertion failed",
+            'pretty_result': "An unexpected error occurred",
             'base_extra_data': {
-                'Error': "{e}"
+                'error': "{e}"
             }
         }
     }
 
-    # Checks the input are valid
-    def check_input(self, inputs: None) -> bool :
-        target = self.config.input ['config'] ['target']
+    def __init__(self):
+        super().__init__()
+        self.error = None
+        self.result_code = None
+        self.target = None
 
-        #Check if the target is not empty and valid
-        assert target is not None and target != "", "The target host is not specified"
-        assert validators.url(target) is not False, "The target host is invalid or malformed"
+    # Set the target
+    def set_input(self, inputs: None) -> bool:
+        target = self.config.input['config']['target']
 
         self.target = target
 
         return True
 
-    #Execute the command curl
-    def execute_command(self, inputs: None) -> bool :
+    # Execute the HTTP request to the target
+    def execute_request(self, inputs: None) -> bool:
         target = self.target
 
-        #Execution of the command
-        sp = subprocess.run(self.CMD_COMMAND.format(host=target).split(" "), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-
-        self.sp_returncode = sp.returncode
-
-        #Casting the stderr from bytes to string in case of error
-        if sp.returncode != 0:
-            self.sp_stderr = sp.stderr.decode("utf-8")
-
-        return True
-
-    #Create the result based on the return code of the subprocess
-    def set_output(self, inputs: None) -> bool :
-        #Variable use to get the result from the RESULT_MAP
-        result_int = int(0)
-
-        #Input errors
-        if self.sp_returncode == 1 or self.sp_returncode == 3:
-            result_int = 4
-
-        #Connection errors
-        elif self.sp_returncode == 6 or self.sp_returncode == 7 or self.sp_returncode == 22 or self.sp_returncode == 28:
-            result_int = 2
-
-        #General error
-        elif self.sp_returncode != 0:
-            result_int = 1
-
-        #Creation of the result
-        result = self.RESULT_MAP.get(result_int)
-        self.result.integer_result = result_int
-        self.result.pretty_result = result['pretty_result']
-
-        #Insert the Error in the result
-        if result_int != 0:
-            result['base_extra_data']['Error'] = self.sp_stderr
-            self.result.base_extra_data['Error'] = self.sp_stderr
+        try:
+            response = request.urlopen(target, timeout=10)
+            self.result_code = response.getcode()
+        except HTTPError as e:
+            self.result_code = e.code
+            self.error = str(e)
+        except URLError as e:
+            self.result_code = result.INTEGER_RESULT_TARGET_CONNECTION_ERROR
+            self.error = str(e.reason)
+        except Exception as e:
+            self.result_code = result.INTEGER_RESULT_MOON_CLOUD_ERROR
+            self.error = str(e)
 
         return True
 
-    #Create input error
-    def assertionErrorInput(self, e) :
+    # Create the result
+    def create_result(self, inputs: None) -> result:
 
-        result_map = self.RESULT_MAP.get(4)
-        result_map['base_extra_data']['Error'] = str(e)
+        if self.result_code == result.INTEGER_RESULT_TARGET_CONNECTION_ERROR:
+            result_integer = result.INTEGER_RESULT_TARGET_CONNECTION_ERROR
+        elif self.result_code == result.INTEGER_RESULT_MOON_CLOUD_ERROR:
+            result_integer = result.INTEGER_RESULT_MOON_CLOUD_ERROR
+        elif self.result_code != 200:
+            result_integer = result.INTEGER_RESULT_FALSE
+        else:
+            result_integer = result.INTEGER_RESULT_TRUE
 
-        return result.Result (
-            integer_result = 4,
-            pretty_result = result_map['pretty_result'],
-            base_extra_data = result_map['base_extra_data']
-        )
+        result_obj = self.RESULT_MAP.get(result_integer)
+        base_extra_data = copy.deepcopy(result_obj['base_extra_data'])
+        if result_integer != result.INTEGER_RESULT_TRUE:
+            base_extra_data['error'] = base_extra_data['error'].format(e=self.error)
+
+        self.result.integer_result = result_integer
+        self.result.pretty_result = result_obj['pretty_result']
+        self.result.base_extra_data = base_extra_data
+
+        return True
 
     def atoms(self) -> typing.Sequence[atom.AtomPairWithException]:
         return [
-            atom.AtomPairWithException(forward=self.check_input, forward_captured_exceptions=[
-                atom.PunctualExceptionInformationForward(
-                    exception_class=AssertionError,
-                    action=atom.OnExceptionActionForward.STOP,
-                    result_producer=self.assertionErrorInput
-                )
-            ]),
-            atom.AtomPairWithException(self.execute_command),
-            atom.AtomPairWithException(self.set_output)
+            atom.AtomPairWithException(self.set_input),
+            atom.AtomPairWithException(self.execute_request),
+            atom.AtomPairWithException(self.create_result)
         ]
+
 
 if __name__ == '__main__':
     entrypoint.start_execution(Probe)
